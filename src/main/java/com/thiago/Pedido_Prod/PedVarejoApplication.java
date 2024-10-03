1ef1cb0306d3beb48bccf331284b424f4e1b8c39
Thiago H O S <thiago2006_sales@hotmail.com>
2024-10-03 05:37:16 -0300

    Iniciando o projeto commit

---
+++ src/main/java/com/thiago/Pedido_Prod/PedVarejoApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.Pedido_Prod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedVarejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedVarejoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/Pedido_Prod/PedVarejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.Pedido_Prod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedVarejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
